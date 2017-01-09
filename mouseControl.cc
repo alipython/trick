a9971bad0720d3e1ffe6742c718917e2dab8c760
alipython <ali.chengjun@gmail.com>
2017-01-09 08:45:05 +0000

    mouse click

---
+++ mouseControl.cc
@@ -0,0 +1,57 @@
+/* *************************************
+*
+* @Author         Noxaean
+* @Email          noxaean@gmail.com
+* @Description  
+* @Created        2017-01-06 15:37:38
+* @Last Modified  2017-01-09 16:38:43
+*
+* ****************************************/
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+
+void freshPage(){
+}
+
+void joinParty(){
+	// join the party
+	SetCursorPos(247,80);
+	mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+	
+	//auto mission
+	
+	SetCursorPos(247,80);
+	mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+}
+
+/*	Define Activity
+ *	10:00 
+ *	12:00
+ *	14:00
+ *	14:30
+ *	15:00
+ *  15:30
+ *  16:00
+ *  16:40
+ *  18:00
+ *  19:00
+ */
+using namespace std;
+int main(){
+	POINT pt;
+	GetCursorPos(&pt);
+	cout << "hehe" << pt.x << "and " << pt.y << endl;	
+	struct tm *timer;
+	time_t timep;
+	time(&timep);
+	timer = localtime(&timep);
+	cout << "h" << timer->tm_hour << "m" << timer->tm_sec << endl;
+	// define 10:00 activity
+	Sleep(5000);
+	cout << "h" << timer->tm_hour << "m" << timer->tm_sec << endl;
+	return 0;
+}
+
+
